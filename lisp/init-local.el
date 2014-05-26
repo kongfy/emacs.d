7acfe7dd8e20c3d7b771d71a0920000d41aadaa5
Fanyu Kong <njukongfy@gmail.com>
2014-05-26 15:36:32 +0800

    开始自定义！

---
+++ lisp/init-local.el
@@ -0,0 +1,12 @@
+;; general
+(global-linum-mode t)
+
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 4)
+
+;; for codec
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; end of init-local.el
+(provide 'init-local)
