3cd7d80805977af939f784d93d2936b332e7e393
Fanyu Kong <njukongfy@gmail.com>
2014-05-26 20:22:25 +0800

    yasnippet

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,12 +1,17 @@
 ;; general
+(add-to-list 'load-path "~/.emacs.d/plugins")
 (global-linum-mode t)
 
-(setq-default indent-tabs-mode nil)
-(setq default-tab-width 4)
+(setq-default c-default-style "bsd"
+              c-basic-offset 4
+              tab-width 4
+              indent-tabs-mode t)
 
 ;; for codec
-(setq default-buffer-file-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+
+(require 'init-yasnippet)
 
 ;; end of init-local.el
 (provide 'init-local)
---
+++ lisp/init-yasnippet.el
@@ -0,0 +1,7 @@
+(require-package 'yasnippet)
+
+;; enable yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(provide 'init-yasnippet)
