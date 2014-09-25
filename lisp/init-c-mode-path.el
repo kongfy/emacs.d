dae906221e4a2c1e3afc04d832e77013dc8b52ce
Fanyu Kong <njukongfy@gmail.com>
2014-09-25 16:07:00 +0800

    delete

--- lisp/init-c-mode-path.el
+++
@@ -1,8 +0,0 @@
-(defun c-mode-path ()
-  (setq flycheck-clang-include-path
-        (list (expand-file-name "~/Documents/Nanos/include/")))
-  )
-
-(add-hook 'c-mode-common-hook 'c-mode-path)
-
-(provide 'init-c-mode-path)
